6fec355523487a0f329e89f43f31e2ff2a505927
Simon Wimmer <swimmer@cs.au.dk>
2023-08-02 18:07:10 +0200

    Add cc deck

--- app/src/Decks.ts
+++ app/src/Decks.ts
@@ -1,9 +1,10 @@
 import events0 from './events.json';
 import events1 from './events1.json';
 import inventions from './inventions.json';
+import climate from './climate.json';
 
 export type event = {
-  year: string,
+  year: string | number,
   event: string,
   timestamp: number,
   difficulty?: number
@@ -12,5 +13,6 @@ export type event = {
 export const decks = [
   {"name": "Basic", "value": events0 as event[]},
   {"name": "Basic 1", "value": events1 as event[]},
-  {"name": "Inventions", "value": inventions as event[]}
+  {"name": "Inventions", "value": inventions as event[]},
+  {"name": "Climate Change", "value": climate as event[]}
 ];
