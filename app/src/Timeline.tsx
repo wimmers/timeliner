40ae5c5e62770e3ab406aeb17146a1a62b15c6e1
Simon Wimmer <swimmer@cs.au.dk>
2023-08-02 16:55:40 +0200

    Timeline: clean and increase click surface

--- app/src/Timeline.tsx
+++ app/src/Timeline.tsx
@@ -5,6 +5,7 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
+import Button from '@mui/material/Button';
 import TimelineOppositeContent, {
   timelineOppositeContentClasses,
 } from '@mui/lab/TimelineOppositeContent';
@@ -15,26 +16,26 @@ function Item({year, event}: any, onClick: () => void, isFirst=false) {
           {year}
         </TimelineOppositeContent>
         <TimelineSeparator>
-          {/* {!isFirst && <TimelineDot />} */}
           <TimelineDot />
           <TimelineConnector
-            onClick={onClick}
-            sx={{'&:hover': {
+            sx={{
+              '&:hover': {
                 width: 10,
-              }}}
-          />
+              },
+            }}
+          >
+            <Button onClick={onClick} sx={{left: -30, height: 40}}/>
+          </TimelineConnector>
         </TimelineSeparator>
         <TimelineContent>{event}</TimelineContent>
       </TimelineItem>
 }
 
 export default function GameTimeline({events, onInsert}: any) {
-    // console.log(events);
     const children = events.map((item: string, index: number) =>
         Item(
             item,
             () => {
-                // console.log("Clicked:", index);
                 onInsert(index);
             }
         )
@@ -49,25 +50,6 @@ export default function GameTimeline({events, onInsert}: any) {
     >
       {Item({year: "", event: ""}, () => onInsert(-1), true)}
       {children}
-      {/* <TimelineItem>
-        <TimelineOppositeContent color="textSecondary">
-          09:30 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent color="textSecondary">
-          10:00 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
-      </TimelineItem> */}
     </Timeline>
   );
 }
